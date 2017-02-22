1c6f5ee79416b9834a6a549b91af4c49fa7a8518
Brian <brianmccarthy1@outlook.ie>
2017-02-22 21:19:17 +0000

    Inital setup of project/ platforms

---
+++ BunnyHop/desktop/src/ie/wit/cgd/bunnyhop/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package ie.wit.cgd.bunnyhop.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import ie.wit.cgd.bunnyhop.BunnyHopMain;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new BunnyHopMain(), config);
+	}
+}
