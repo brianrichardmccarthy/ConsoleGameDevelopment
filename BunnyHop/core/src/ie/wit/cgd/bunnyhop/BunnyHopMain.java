6ce56aefc17e0fc289cc5e3b33aa797654605af4
Brian <brianmccarthy1@outlook.ie>
2017-02-23 15:41:55 +0000

    Step 2: Building the Game Loop

--- BunnyHop/core/src/ie/wit/cgd/bunnyhop/BunnyHopMain.java
+++ BunnyHop/core/src/ie/wit/cgd/bunnyhop/BunnyHopMain.java
@@ -1,37 +1,75 @@
 package ie.wit.cgd.bunnyhop;
 
+import com.badlogic.gdx.Application;
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
 
 import ie.wit.cgd.bunnyhop.game.WorldController;
 import ie.wit.cgd.bunnyhop.game.WorldRenderer;
 
 public class BunnyHopMain extends ApplicationAdapter {
-	private static final String TAG = BunnyHopMain.class.getName();
 
-	private WorldController worldController;
-	private WorldRenderer worldRenderer;
+    private static final String TAG = BunnyHopMain.class.getName();
 
-	@Override
-	public void create() {
-	}
+    private WorldController worldController;
+    private WorldRenderer worldRenderer;
+    private boolean paused;
 
-	@Override
-	public void render() {
-	}
+    @Override
+    public void create() {
 
-	@Override
-	public void resize(int width, int height) {
-	}
+        // Set Libgdx log level to DEBUG
+        Gdx.app.setLogLevel(Application.LOG_DEBUG);
 
-	@Override
-	public void pause() {
-	}
+        // Initialize controller and renderer
+        worldController = new WorldController();
+        worldRenderer = new WorldRenderer(worldController);
 
-	@Override
-	public void resume() {
-	}
+        // Game world is active on start
+        paused = false;
+    }
 
-	@Override
-	public void dispose() {
-	}
+    @Override
+    public void render() {
+
+        // Do not update game world when paused.
+        if (!paused) {
+            // Update game world by the time that has passed since last rendered frame.
+            worldController.update(Gdx.graphics.getDeltaTime());
+
+            // Sets the clear screen color to: Cornflower Blue
+            Gdx.gl.glClearColor(0x64 / 255.0f, 0x95 / 255.0f, 0xed / 255.0f, 0xff / 255.0f);
+
+            // Clears the screen
+            Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+            // Render game world to screen
+            worldRenderer.render();
+        }
+    }
+
+    @Override
+    public void resize(int width, int height) {
+
+        worldRenderer.resize(width, height);
+    }
+
+    @Override
+    public void pause() {
+
+        paused = true;
+    }
+
+    @Override
+    public void resume() {
+
+        paused = false;
+    }
+
+    @Override
+    public void dispose() {
+
+        worldRenderer.dispose();
+    }
 }
