4f79cc10c936e13d0382b7fd043fde9e89245ade
Brian <brianmccarthy1@outlook.ie>
2017-02-23 18:25:44 +0000

    Reset project to fix missing dependences

--- BunnyHop/desktop/src/ie/wit/cgd/bunnyhop/desktop/DesktopLauncher.java
+++ BunnyHop/desktop/src/ie/wit/cgd/bunnyhop/desktop/DesktopLauncher.java
@@ -2,11 +2,30 @@ package ie.wit.cgd.bunnyhop.desktop;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.badlogic.gdx.tools.texturepacker.TexturePacker;
+import com.badlogic.gdx.tools.texturepacker.TexturePacker.Settings;
+
 import ie.wit.cgd.bunnyhop.BunnyHopMain;
 
 public class DesktopLauncher {
-	public static void main (String[] arg) {
-		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		new LwjglApplication(new BunnyHopMain(), config);
-	}
+
+    private static boolean rebuildAtlas = true;
+    private static boolean drawDebugOutline = true;
+
+    public static void main(String[] args) {
+
+        if (rebuildAtlas) {
+            Settings settings = new Settings();
+            settings.maxWidth = 1024;
+            settings.maxHeight = 1024;
+            settings.debug = drawDebugOutline;
+            TexturePacker.process(settings, "assets-raw/images", "../android/assets/images", "bunnyhop.atlas");
+        }
+
+        LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+        config.title = "BunnyHop";
+        config.width = 800;
+        config.height = 480;
+        new LwjglApplication(new BunnyHopMain(), config);
+    }
 }
