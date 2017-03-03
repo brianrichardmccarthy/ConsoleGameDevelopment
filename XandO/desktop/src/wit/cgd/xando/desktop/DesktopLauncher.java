912d7a2ca3b1ca19d33f0f213b26c664de36e531
Brian <brianmccarthy1@outlook.ie>
2017-03-03 11:58:57 +0000

    Started X and 0 assignment

---
+++ XandO/desktop/src/wit/cgd/xando/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package wit.cgd.xando.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import wit.cgd.xando.XandOMain;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new XandOMain(), config);
+	}
+}
