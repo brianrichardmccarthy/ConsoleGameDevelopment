eaa66debfb4279e9f0378f27e1f9d43f14836713
Brian <brianmccarthy1@outlook.ie>
2017-03-03 12:20:34 +0000

    Setup of core classes

---
+++ XandO/core/src/wit/cgd/xando/game/WorldRenderer.java
@@ -0,0 +1,45 @@
+package wit.cgd.xando.game;
+
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.utils.Disposable;
+
+import wit.cgd.xando.game.util.Constants;
+
+public class WorldRenderer implements Disposable {
+
+	private static final String TAG = WorldRenderer.class.getName();
+	public OrthographicCamera camera;
+	private SpriteBatch batch;
+	private WorldController worldController;
+
+	public WorldRenderer(WorldController worldController) {
+		this.worldController = worldController;
+		init();
+	}
+
+	private void init() {
+		batch = new SpriteBatch();
+		camera = new OrthographicCamera(Constants.VIEWPORT_WIDTH, Constants.VIEWPORT_HEIGHT);
+		camera.position.set(0, 0, 0);
+	}
+
+	public void resize(int width, int height) {
+
+		camera.viewportWidth = (Constants.VIEWPORT_HEIGHT / height) * width;
+		camera.update();
+	}
+
+	public void render() {
+		batch.setProjectionMatrix(camera.combined);
+		batch.begin();
+
+		batch.end();
+	}
+
+	@Override
+	public void dispose() {
+
+		batch.dispose();
+	}
+}
